676d09b2c2a92951b9bfad44c6ef4d68dc385e4d
zhendong zhao <zhendong zhao>
2017-03-03 16:38:54 +0800

    Initial commit

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * Created by master on 2017/3/3.
+ * 用于测试idea的使用情况
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+       System.out.println("测试 world ····");//测试纪录
+
+    }
+
+}
